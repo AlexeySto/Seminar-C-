26cdeb2be81afd8b4eb50fa73e555fb381f504ae
Алексей <stolyarov1986@inbox.ru>
2023-07-06 16:52:12 +0300

    added

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number * number;
+Console.Write("Квадрат числа: " + result);
